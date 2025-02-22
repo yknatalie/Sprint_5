4ab4713479f0bbf7fe6cbd1fb07a3bd77815f609
Natalia Ivanova <djeini@mail.ru>
2025-02-22 19:05:08 +0300

    Sign in fields changed

--- locators.py
+++ locators.py
@@ -15,8 +15,8 @@ class Locators:
 
     #Страница авторизации
     ENTER_TEXT = (By.XPATH, "//h2[text() = 'Вход']")
-    INPUT_EMAIL_AUTH = (By.XPATH, ".//fieldset[1]/div/div/input")
-    INPUT_PASSWORD_AUTH = (By.XPATH, ".//fieldset[2]/div/div/input")
+    INPUT_EMAIL_AUTH = (By.XPATH, "//input[@name='name']")
+    INPUT_PASSWORD_AUTH = (By.XPATH, "//input[@name='Пароль']")
     ENTER_BUTTON = (By.XPATH, ".//button[text()= 'Войти']")
     ENTER_LINK = (By.XPATH, "//a[@href = '/login']")
     FORGET_PASSWORD_LINK = (By.LINK_TEXT, "Восстановить пароль")
